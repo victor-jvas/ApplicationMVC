a5db9aeece6e743017c4df077b32ef378c8250b2
jVictor <victor.jvas1@gmail.com>
2021-05-03 22:50:17 -0300

    Setting up Entity Framework

---
+++ DIOApplicationMVC/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace DIOApplicationMVC.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ DIOApplicationMVC/Models/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DIOApplicationMVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-3B14NSU\SQLEXPRESS;Database=Appmvc;Integrated Security=True");
+        }
+    }
+}
